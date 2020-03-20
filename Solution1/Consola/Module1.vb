3d5174b6cd86c766a88e031b246b403fdf2b69e0
Rodrigo <rpereira990@gmail.com>
2020-03-20 04:23:42 -0300

    asdasdasd

--- Solution1/Consola/Module1.vb
+++ Solution1/Consola/Module1.vb
@@ -4,9 +4,20 @@ Module Module1
 
     Sub Main()
 
-        Dim op As Integer
         Dim emp As New Empleado
         Dim cal As New Calculo
+        Dim ver As New Verificaciones
+        Dim op As Integer
+        Dim ced As String
+        Dim repCed As Boolean = False
+        Dim nom As String
+        Dim ape As String
+        Dim tel As Integer
+        Dim dire As String
+        Dim sueldo As Double
+        Dim opcion As String
+        Dim tipo As Integer
+        Const numeros = "0123456789"
 
         Do While op <> 3
 
@@ -21,50 +32,192 @@ Module Module1
 
                 Case 1
                     Console.Clear()
+
                     Try
                         Console.Write("Cédula del empleado: ")
-                        Dim ced As String = Console.ReadLine()
-                        Dim rep As Boolean = False
-                        If (ced.Length = 7 Or ced.Length = 8) Then
-                            emp._cedula.Add(ced)
-                        Else
-                            Console.Write("Debe ingresar una cédula válida(pulse cualquier tecla)")
-                            Console.ReadLine()
+                        ced = Console.ReadLine()
+                        If ver.verificarCedula(ced, emp, repCed) = False Then
                             Exit Select
                         End If
+
+
                         Console.Write("Nombre del empleado: ")
-                        emp._nombre.Add(Console.ReadLine.ToUpper)
+                        nom = Console.ReadLine()
+                        If nom.IndexOfAny(numeros.ToArray) > -1 Then
+                            Console.Write("Ha habido un error al ingresar los datos. ¿Desea repetir? (s/n): ")
+                            opcion = Console.ReadLine()
+                            If (opcion = "s") Then
+                                Console.Write("Nombre del empleado: ")
+                                Dim nom2 As String = Console.ReadLine()
+                                If nom2.IndexOfAny(numeros.ToArray) > -1 Then
+                                    ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                    Console.WriteLine("")
+                                    Console.Write("No debe contener caracteres numéricos (pulse cualquier tecla para volver al menú)")
+                                    Console.ReadLine()
+                                    Exit Select
+                                Else
+                                    emp._nombre.Add(nom2.ToUpper)
+                                End If
+                            ElseIf (opcion = "n") Then
+                                ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                Exit Select
+                            End If
+                        Else
+                            emp._nombre.Add(nom.ToUpper)
+                        End If
+
+
                         Console.Write("Apellido del empleado: ")
-                        emp._apellido.Add(Console.ReadLine.ToUpper)
+                        ape = Console.ReadLine()
+                        If ape.IndexOfAny(numeros.ToArray) > -1 Then
+                            Console.Write("Ha habido un error al ingresar los datos. ¿Desea repetir? (s/n): ")
+                            opcion = Console.ReadLine()
+                            If (opcion = "s") Then
+                                Console.Write("Apellido del empleado: ")
+                                Dim ape2 As String = Console.ReadLine()
+                                If ape2.IndexOfAny(numeros.ToArray) > -1 Then
+                                    ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                    Console.WriteLine("")
+                                    Console.Write("No debe contener caracteres numéricos (pulse cualquier tecla para volver al menú)")
+                                    Console.ReadLine()
+                                    Exit Select
+                                Else
+                                    emp._apellido.Add(ape2.ToUpper)
+                                End If
+                            ElseIf (opcion = "n") Then
+                                ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                Exit Select
+                            End If
+                        Else
+                            emp._apellido.Add(ape.ToUpper)
+                        End If
+
+
+                        Console.Write("Teléfono del empleado: ")
+                        tel = Val(Console.ReadLine)
+                        If (IsNumeric(tel)) Then
+                            emp._telefono.Add(tel)
+                        Else
+                            Console.Write("Error al ingresar los datos. ¿Desea repetir? (s/n): ")
+                            opcion = Console.ReadLine()
+                            If (opcion = "s") Then
+                                Console.Write("Teléfono del empleado: ")
+                                Dim tel2 As Integer = Console.ReadLine()
+                                If IsNumeric(tel2) Then
+                                    emp._telefono.Add(tel2)
+                                Else
+                                    ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                    Console.WriteLine("")
+                                    Console.Write("Error al ingresar el dato (pulse cualquier tecla para volver al menú)")
+                                    Console.ReadLine()
+                                    Exit Select
+                                End If
+                            ElseIf (opcion = "n") Then
+                                ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                Exit Select
+                            End If
+                        End If
+
+
                         Console.Write("Dirección del empleado: ")
                         emp._direccion.Add(Console.ReadLine.ToUpper)
-                        Console.Write("Teléfono del empleado: ")
-                        emp._telefono.Add(Console.ReadLine.ToUpper)
-                        Console.Write("Sueldo base del empleado: ")
-                        emp._sueldo.Add(Console.ReadLine)
-                        Console.Write("Tipo de empleado(1.Gerente 2.Operario 3.Administrativo): ")
-                        emp._tipoEmpleado.Add(Console.ReadLine)
-                        cal.MontoIndividual(emp)
-                    Catch ex As Exception
 
+
+                        Console.Write("Sueldo base del empleado: ")
+                        sueldo = Val(Console.ReadLine)
+                        If IsNumeric(sueldo) Then
+                            emp._sueldo.Add(sueldo)
+                        Else
+                            Console.Write("Error al ingresar los datos. ¿Desea repetir? (s/n): ")
+                            opcion = Console.ReadLine
+                            If (opcion = "s") Then
+                                Console.Write("Sueldo base del empleado: ")
+                                Dim sueldo2 As Double = Double.Parse(Console.ReadLine())
+                                If IsNumeric(sueldo2) Then
+                                    emp._sueldo.Add(sueldo2)
+                                Else
+                                    ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                    Console.WriteLine("")
+                                    Console.Write("Error al ingresar el dato (pulse cualquier tecla para volver al menú)")
+                                    Console.ReadLine()
+                                    Exit Select
+                                End If
+                            ElseIf (opcion = "n") Then
+                                ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                Exit Select
+                            End If
+                        End If
+
+
+                        Console.Write("Tipo de empleado(1.Gerente 2.Operario 3.Administrativo): ")
+                        tipo = Console.ReadLine
+                        If (tipo = 1 Or tipo = 2 Or tipo = 3) Then
+                            emp._tipoEmpleado.Add(tipo)
+                            cal.MontoIndividual(emp)
+                        Else
+                            Console.Write("Error al ingresar los datos. ¿Desea repetir? (s/n): ")
+                            opcion = Console.ReadLine
+                            If (opcion = "s") Then
+                                Console.Write("Tipo de empleado(1.Gerente 2.Operario 3.Administrativo): ")
+                                Dim tipo2 As Integer = Console.ReadLine
+                                If (tipo2 = 1 Or tipo2 = 2 Or tipo2 = 3) Then
+                                    emp._tipoEmpleado.Add(tipo)
+                                    cal.MontoIndividual(emp)
+                                Else
+                                    ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                    Console.WriteLine("")
+                                    Console.Write("Error al ingresar el dato (pulse cualquier tecla para volver al menú)")
+                                    Console.ReadLine()
+                                    Exit Select
+                                End If
+                            ElseIf (opcion = "n") Then
+                                ver.borrarDatos(emp, nom, ced, ape, dire, tel, sueldo, tipo)
+                                Exit Select
+                            End If
+                        End If
+
+                    Catch ex As Exception
+                        Console.Write("Ocurrió un error inesperado")
+                        Console.ReadLine()
+                        Exit Select
                     End Try
 
                 Case 2
                     Console.Clear()
+                    Dim mostrarTipo As String = ""
+                    Console.WriteLine("Cédula | Nombre | Apellido | Teléfono | Dirección | Sueldo base | Tipo de funcionario | Sueldo total")
                     For i As Integer = 0 To emp._cedula.Count - 1
-
-                        Console.WriteLine(emp._cedula.Item(i) & " | " & emp._nombre.Item(i) & " | " & emp._apellido.Item(i) & " | " & emp._sueldo.Item(i) & " | " & emp._tipoEmpleado.Item(i) & " | " & "Sueldo total: " & emp._sueldoTotal.Item(i))
+                        If (emp._tipoEmpleado.Item(i) = 1) Then
+                            mostrarTipo = "Gerente"
+                        ElseIf (emp._tipoEmpleado.Item(i) = 2) Then
+                            mostrarTipo = "Operario"
+                        ElseIf (emp._tipoEmpleado.Item(i) = 3) Then
+                            mostrarTipo = "Administrativo"
+                        End If
+                        Console.WriteLine("")
+                        Console.Write(emp._cedula.Item(i) + " | ")
+                        Console.Write(emp._nombre.Item(i) + " | ")
+                        Console.Write(emp._apellido.Item(i) + " | ")
+                        Console.Write(emp._telefono.Item(i) & " | ")
+                        Console.Write(emp._direccion.Item(i) + " | ")
+                        Console.Write(emp._sueldo.Item(i) & " | ")
+                        Console.Write(mostrarTipo + " | ")
+                        Console.Write(emp._sueldoTotal.Item(i))
 
                     Next
                     If (emp._cedula.Count <> 0) Then
                         Console.WriteLine("")
-                        Console.WriteLine("El monto total a pagar es: " & cal.MontoTotal(emp))
+                        Console.WriteLine("El monto total a pagarle a todos los funcionarios es: " & cal.MontoTotal(emp))
                     Else
                         Console.Write("No hay datos para mostrar")
+                        Console.WriteLine("")
                     End If
                     Console.WriteLine("")
                     Console.WriteLine("Pulse cualquier tecla para volver")
                     Console.ReadLine()
+
+                Case 3
+                    Exit Do
 
                 Case Else
                     Console.Write("Ingresó una opción incorrecta")
---
+++ Solution1/Logica/Verificaciones.vb
@@ -0,0 +1,69 @@
+﻿Public Class Verificaciones
+
+
+    Public Function verificarCedula(ced As String, emp As Empleado, repCed As Boolean) As Boolean
+
+        If (ced.Length = 7 Or ced.Length = 8) Then
+            For i As Integer = 0 To emp._cedula.Count - 1
+                If (emp._cedula.Item(i) = ced) Then
+                    repCed = True
+                    Console.WriteLine("")
+                    Console.Write("Esta cédula ya fue ingresada (pulse cualquier tecla para volver al menú)")
+                    Console.ReadLine()
+                    Return False
+                End If
+            Next
+            If repCed = False Then
+                emp._cedula.Add(ced)
+                Return True
+            End If
+        Else
+            Console.WriteLine("")
+            Console.Write("Debe ingresar una cédula válida (pulse cualquier tecla)")
+            Console.ReadLine()
+            Return False
+        End If
+        Return False
+    End Function
+
+    Public Function borrarDatos(emp As Empleado, nom As String, ced As String, ape As String, dire As String, tel As Integer, sueldo As Double, tipo As Integer) As Boolean
+
+        For i As Integer = 0 To emp._cedula.Count - 1
+            If (emp._cedula.Item(i) = ced) Then
+                emp._cedula.RemoveAt(i)
+            End If
+        Next
+        For i As Integer = 0 To emp._nombre.Count - 1
+            If (emp._nombre.Item(i) = nom.ToUpper) Then
+                emp._nombre.RemoveAt(i)
+            End If
+        Next
+        For i As Integer = 0 To emp._apellido.Count - 1
+            If (emp._apellido.Item(i) = ape.ToUpper) Then
+                emp._apellido.RemoveAt(i)
+            End If
+        Next
+        For i As Integer = 0 To emp._direccion.Count - 1
+            If (emp._direccion.Item(i) = dire.ToUpper) Then
+                emp._direccion.RemoveAt(i)
+            End If
+        Next
+        For i As Integer = 0 To emp._telefono.Count - 1
+            If (emp._telefono.Item(i) = tel) Then
+                emp._telefono.RemoveAt(i)
+            End If
+        Next
+        For i As Integer = 0 To emp._sueldo.Count - 1
+            If (emp._sueldo.Item(i) = sueldo) Then
+                emp._sueldo.RemoveAt(i)
+            End If
+        Next
+        For i As Integer = 0 To emp._tipoEmpleado.Count - 1
+            If (emp._tipoEmpleado.Item(i) = tipo) Then
+                emp._tipoEmpleado.RemoveAt(i)
+            End If
+        Next
+        Return True
+    End Function
+
+End Class
